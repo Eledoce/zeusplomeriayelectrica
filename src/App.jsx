f59132e2984b6c06673c0480e92539e12cdd69a2
Angel Ramirez <miguelangel8877@outlook.com>
2023-10-20 12:37:10 -0600

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className="h-screen w-screen relative text-white bg-gray-900">App</div>
+    <div className="h-screen w-screen relative text-white bg-gray-900">
+      Zeus
+    </div>
   )
 }
